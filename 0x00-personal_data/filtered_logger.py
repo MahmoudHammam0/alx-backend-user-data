e3d4a339c490f7399d4d5d95ebc2a81909497931
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-04 11:22:38 -0700

    2. Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,7 +10,7 @@ PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 def get_logger() -> logging.Logger:
     """ return a logger object to handle PII """
-    logger = logging.getLogger('user_data')
+    logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
     handler = logger.StreamHandler()
