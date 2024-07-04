1b1d1872743930e95e5f2c4800a3fb35f27f04bb
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-04 11:24:59 -0700

    2. Create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,15 @@ import logging
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """ returns the log message """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
+
+
 def get_logger() -> logging.Logger:
     """ return a logger object to handle PII """
     logger = logging.getLogger("user_data")
@@ -37,12 +46,3 @@ class RedactingFormatter(logging.Formatter):
         record.msg = filter_datum(self.fields, self.REDACTION,
                                   record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """ returns the log message """
-    for field in fields:
-        message = re.sub(f'{field}=.*?{separator}',
-                         f'{field}={redaction}{separator}', message)
-    return message
