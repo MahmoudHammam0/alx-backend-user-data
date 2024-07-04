7e34247f394aed25e7d784cdd39f222145d3e86a
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-04 12:52:51 -0700

    6. Check valid password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -7,3 +7,9 @@ def hash_password(password: str) -> bytes:
     """ hash the password before store in db """
     hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """  validate that the provided password matches the hashed password """
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
