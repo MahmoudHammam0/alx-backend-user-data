2be7ce3b876c31ef836df5ac6ad05ee145faa901
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-11 16:57:51 -0700

    10. Sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 
 class SessionDBAuth(SessionExpAuth):
     """ SessionDBAuth class """
-    def create_session(self, user_id=None):
+    def create_session(self, user_id=None) -> str:
         """ overload of create session but with UserSession """
         session_id = super().create_session(user_id)
         if not session_id or type(session_id) != str:
@@ -17,7 +17,7 @@ class SessionDBAuth(SessionExpAuth):
         user.save()
         return session_id
 
-    def user_id_for_session_id(self, session_id=None):
+    def user_id_for_session_id(self, session_id=None) -> str:
         """ returns the User ID by requesting UserSession in the database """
         try:
             users_list = UserSession.search({"session_id": session_id})
@@ -32,7 +32,7 @@ class SessionDBAuth(SessionExpAuth):
 
         return users_list[0].get('user_id')
 
-    def destroy_session(self, request=None):
+    def destroy_session(self, request=None) -> bool:
         """ remove the session based on the session id """
         session_id = self.session_cookie(request)
         try:
