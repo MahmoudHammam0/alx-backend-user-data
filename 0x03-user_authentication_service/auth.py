64a561480e3affe155aef56ca559816e3da1cb6f
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-16 19:43:46 -0700

    11. Log in

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -56,9 +56,7 @@ class Auth:
             user = self._db.find_user_by(email=email)
         except Exception:
             return None
-        session_id = _generate_uuid()
-        try:
-            self._db.update_user(user.id, session_id=session_id)
-        except Exception:
-            return None
-        return session_id
+        else:
+            session_id = _generate_uuid()
+            user.session_id = session_id
+            return session_id
