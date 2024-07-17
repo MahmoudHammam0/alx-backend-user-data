44bcdfb36f794f7575fabd44109610fdf527e3d7
MahmoudHammam0 <Mahmoudhammam235@gmail.com>
2024-07-16 17:05:14 -0700

    5. Register user

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 import bcrypt
 from db import DB
 from user import User
+from typing import Union
 
 
 def _hash_password(password: str) -> bytes:
@@ -21,7 +22,7 @@ class Auth:
         """ initialization """
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> User:
+    def register_user(self, email: str, password: str) -> Union[None, User]:
         """ register a new user and return a User object """
         session = self._db._session
         user = session.query(User).filter_by(email=email).first()
